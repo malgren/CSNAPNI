3807530619780b2799ca83aac11e530427c78d1d
Mikaela Algren <malgren@mines.edu>
2021-01-21 11:53:21 -0700

    improves estimates of temporal variation in P feed supplementation

--- CreateInputsSubs/P_diet_supp.r
+++ CreateInputsSubs/P_diet_supp.r
@@ -1,14 +1,16 @@
 #P_diet_supp.r
 #This script determines to what extent the crop allocations to animals
-#in the N-requirement-based diet model fulfill the P needs of animals
-#and uses this information to estimate P supplementation in animal diets
+#in the N-intake-based diet model fulfill the P needs of animals
+#and uses this information to estimate P supplementation and animal protein feed consumption in animal diets
 
 #Be sure to run constants.r and cropalloc_model.r first if running this script independently
 #source('constants.r')
 #source('cropalloc_model.r')
 
-#use livestock P requirements (animPreq)
-#and modeled allocation of each crop to each animal type in each year (cropPtoanim)
+#use livestock P requirements (animPreq) 
+#-- contrary to what the variable name suggests, this is a P intake estimate, not animal P needs --
+#and N-intake based allocation of each crop to each animal type in each year (cropPtoanim)
+#to calculate the diet P deficit
 
 missingPperanim = array(0, c(n_anims,data_yrs))
 missingPanimtotals = array(0, c(n_anims,data_yrs))
@@ -18,42 +20,17 @@ for(i in 1:data_yrs){
   #remove negative values (P overallocations)
   missingPperanim[missingPperanim[,i]<0,i] = 0
   missingPanimtotals[,i] = missingPperanim[,i]*animpoptotal[,i]
+  anim_prot_feed_P = animPreq #- manure_P - meat_P - waste_P_from_slaughter
 }
 
 missingPtotal = colSums(missingPanimtotals) # total missing P in each year
 
-#check P availability for P supplements
-file_name = paste("RawData/NAPI_tbx_totals.txt",sep = "")
-NAPI_tbx_totals=t(array(scan(file_name), c(data_yrs,4)))
-US_P_fertilizer_sales = NAPI_tbx_totals[1,]*10^9
-US_P_inputs = c(4.2,4.2,4.2,4.2,3.6)*10^9 #in kg P, 2012 only (mining+imports, converted from Phosphate rock to P)
-prop_P_to_phosphoric_acid = 0.95 #from USGS, 2002-2017 data says "more than 95%"
-#assuming all P ore, domestic or imported, has the same use distribution
-fertilizer_exports = c(1.29,1.29,1.29,1.29,0.85)*10^9 #in kg P, using 2012 and 2017 data only
-phosphoric_acid_exports = c(0.2,0.2,0.2,0.2,0.13)*10^9 #in kg P, using 2012 and 2017 data only
-P_to_phosphoric_acid = US_P_inputs*prop_P_to_phosphoric_acid
-P_waste = 0.10*P_to_phosphoric_acid
-P_to_feed_supplements = P_to_phosphoric_acid-US_P_fertilizer_sales-fertilizer_exports-phosphoric_acid_exports-P_waste
-
-prop_P_need_avail = P_to_feed_supplements/missingPtotal
-
-#factor-in P digestibility
-P_digestibility = array(scan("RawData/P_digestibility.txt"), c(19,19))
-missingPperanim_dig = array(0, c(n_anims,data_yrs))
-missingPanimtotals_dig = array(0, c(n_anims,data_yrs))
-cropPtoanim_dig = cropPtoanim * array(P_digestibility,c(19,19,data_yrs))
-for(i in 1:data_yrs){
-  missingPperanim_dig[,i] = (animPreq-rowSums(cropPtoanim_dig[,,i]))
-  #remove negative values (P overallocations)
-  missingPperanim_dig[missingPperanim_dig[,i]<0,i] = 0
-  missingPanimtotals_dig[,i] = missingPperanim_dig[,i]*animpoptotal[,i]
-}
-missingPtotal_dig = colSums(missingPanimtotals_dig) # total missing P in each year
-
-prop_digP_need_avail = P_to_feed_supplements/missingPtotal_dig
+#result of calculations in Pfeedsupplementprod.xlsx
+mineral_P_to_feed_supplements = c(1.048,0.684,0.353,0.683,0.924)*10^9
+prop_P_need_avail = mineral_P_to_feed_supplements/missingPtotal
 
 for(i in 1:data_yrs){
-  Psupp_peranim[,i] = missingPperanim_dig[,i]*prop_digP_need_avail[i]
+  Psupp_peranim[,i] = missingPperanim[,i]*prop_P_need_avail[i]
   Psupp_animtotals[,i] = Psupp_peranim[,i]*animpoptotal[,i]
 }
 Psupp_total = colSums(Psupp_animtotals)
